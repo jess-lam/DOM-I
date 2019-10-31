e1b99bc1b2250aae1a52c7bed8b33fe2a0f5379c
Jessica Lam <jessicalam@jessicas-air-2.lan>
2019-10-30 23:38:59 -0700

    completed the page using js file

--- js/index.js
+++ js/index.js
@@ -40,3 +40,64 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const navigation = document.querySelectorAll('a')
+navigation[0].textContent = "Services"
+navigation[1].textContent = "Product"
+navigation[2].textContent = "Vision"
+navigation[3].textContent = "Features"
+navigation[4].textContent = "About"
+navigation[5].textContent = "Contact"
+
+navigation[0].style.color = 'green';
+navigation[1].style.color = 'green';
+navigation[2].style.color = 'green';
+navigation[3].style.color = 'green';
+navigation[4].style.color = 'green';
+navigation[5].style.color = 'green';
+
+const team = document.createElement('a')
+team.href = "#"
+team.textContent = "Team"
+
+const shop = document.createElement('a')
+shop.href = "#"
+shop.textContent = "Shop"
+
+const navBar = document.querySelector("nav")
+navBar.appendChild(team);
+navBar.prepend(shop);
+team.style.color = 'green';
+shop.style.color = 'green';
+
+
+const title = document.querySelector('h1')
+title.textContent = "DOM Is Awesome"
+
+const button = document.querySelector('button')
+button.textContent = "Get Started"
+
+const headerImg = document.getElementById('cta-img')
+headerImg.src = "img/header-img.png"
+
+const features = document.querySelectorAll('h4')
+features[0].textContent = "Features"
+features[1].textContent = "About"
+features[2].textContent = "Services"
+features[3].textContent = "Product"
+features[4].textContent = "Vision"
+features[5].textContent = "Contact"
+
+const contentParagraph = document.getElementsByTagName('p');
+contentParagraph[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+contentParagraph[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+contentParagraph[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+contentParagraph[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+contentParagraph[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+contentParagraph[5].textContent = "123 Way 456 Street Somewhere, USA"
+contentParagraph[6].textContent = "1 (888) 888-8888"
+contentParagraph[7].textContent = "sales@greatidea.io"
+contentParagraph[8].textContent = "Copyright Great Idea! 2018"
+
+const middleImg = document.getElementById('middle-img')
+middleImg.src = "img/mid-page-accent.jpg"
